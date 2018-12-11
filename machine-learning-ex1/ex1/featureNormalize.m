9e6ff493da8a878efaffc093819fc76ec4597444
outpaul <bitanpaul8@gmail.com>
2018-12-11 16:13:08 +0530

    ex1 Complete

--- machine-learning-ex1/ex1/featureNormalize.m
+++ machine-learning-ex1/ex1/featureNormalize.m
@@ -13,6 +13,6 @@ sigma = zeros(1, size(X, 2));
 for i=1:size(X,2),
     mu(i) = mean(X(:,i));
     sigma(i) = std(X(:,i));
-    X(:,i) = (X(:,i) - mu(i))/sigma(i);
+    X_norm(:,i) = (X(:,i) - mu(i))/sigma(i);
 end;
 end;
